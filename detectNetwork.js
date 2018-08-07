d484fd29e5cf1ed0e1eb8bd24302c61c62c5a7c0
Yuki <yukiej@gmail.com>
2018-08-07 07:02:42 -0700

    First commit for MasterCard, Visa, American Express, and Diner's Club cards.

--- detectNetwork.js
+++ detectNetwork.js
@@ -13,6 +13,30 @@ var detectNetwork = function(cardNumber) {
   // The American Express network always starts with a 34 or 37 and is 15 digits long
 
   // Once you've read this, go ahead and try to implement this function, then return to the console.
+
+  //check length of string and first two digits
+  var numDigits = cardNumber.length
+  var startingDigits = cardNumber.slice(0,2)
+
+  //Diner's club: Check if numDigits is 14 digits and starts with 38 or 39
+  if (numDigits == 14 && (startingDigits == 38 || startingDigits == 39)){
+  	return 'Diner\'s Club';
+  }
+
+ //American Express: Check if numDigits is 15 digits and starts with 34 or 37
+  if (numDigits == 15 && (startingDigits == 34 || startingDigits == 37)){
+  	return 'American Express';
+  }
+
+  //Visa: Check if numDigits is 13, 16, or 19 digits and starts with 4
+  if ([13,16,19].includes(Number(numDigits)) && startingDigits[0]==4){
+  	return 'Visa';
+  }
+
+  //MasterCard: Check if numDigits is 16 digits and starts with 51,52,53,54,or 55
+  if (numDigits == 16 && [51,52,53,54,55].includes(Number(startingDigits))){
+  	return 'MasterCard';
+  }
 };
 
 
