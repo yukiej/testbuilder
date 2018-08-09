45b2f17a94fea485cf19c740beed1924a674dc16
Yuki <yukiej@gmail.com>
2018-08-08 22:31:35 -0700

    Add Maestro card

--- detectNetwork.js
+++ detectNetwork.js
@@ -14,9 +14,10 @@ var detectNetwork = function(cardNumber) {
 
   // Once you've read this, go ahead and try to implement this function, then return to the console.
 
-  //check length of string and first two digits
-  var numDigits = cardNumber.length
-  var startingDigits = cardNumber.slice(0,2)
+  //check length of string and check first two digits
+  var numDigits = cardNumber.length;
+  var startingDigits = Number(cardNumber.slice(0,2));
+  var firstDigit = Number(cardNumber[0]);
 
   //Diner's club: Check if numDigits is 14 digits and starts with 38 or 39
   if (numDigits == 14 && (startingDigits == 38 || startingDigits == 39)){
@@ -28,15 +29,27 @@ var detectNetwork = function(cardNumber) {
   	return 'American Express';
   }
 
-  //Visa: Check if numDigits is 13, 16, or 19 digits and starts with 4
-  if ([13,16,19].includes(Number(numDigits)) && startingDigits[0]==4){
+  //Visa: Check if numDigits is 13, 16, or 19 digits and card number starts with 4
+  if ([13,16,19].includes(numDigits) && firstDigit == 4){
   	return 'Visa';
   }
 
   //MasterCard: Check if numDigits is 16 digits and starts with 51,52,53,54,or 55
-  if (numDigits == 16 && [51,52,53,54,55].includes(Number(startingDigits))){
+  if (numDigits == 16 && [51,52,53,54,55].includes(startingDigits)){
   	return 'MasterCard';
   }
-};
+
+  //Discover: Check for prefix of 6011, 644-649, or 65, and a length of 16 or 19.
+  //if ([16,19].includes(numDigits) && [60, 64, 65].includes(startingDigits)){
+ // 	if (cardNumber.slice(0,5) == 6011){
+
+  //	}
+  //}
+
+  //Maestro always has a prefix of 5018, 5020, 5038, or 6304, and a length of 12-19.
+  if ((numDigits >= 12 && numDigits <= 19) && [5018, 5020, 5038, 6304].includes(Number(cardNumber.slice(0,4)))){
+  	return 'Maestro';
+  }
+}
 
 
